3608785ae4dfef799d68d2bb4c6c94f7c598fe29
Chandra Mishra <prasadchandra18@gmail.com>
2019-05-05 11:36:46 +0530

    learning

---
+++ homePage1/script.js
@@ -0,0 +1,28 @@
+         function myFunction() {
+        var email = document.getElementById("email");
+        var password = document.getElementById("password");
+        var filter1 = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;     
+        var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if ((!filter.test(email.value)) && (!filter1.test(password.value))) {
+            
+            document.getElementById("message").innerHTML = "invalid Email";
+            document.getElementById("message1").innerHTML = "password is not strong please provide uppercase,lowercase,special characters,digits and length of password is atleast eight";
+            return false;
+        }
+             else if(!filter.test(email.value)){
+                document.getElementById("message").innerHTML = "invalid Email";
+                document.getElementById("email").innerHTML = ""; 
+                 return false;
+             }
+             else if(!filter1.test(password.value)){
+                document.getElementById("message1").innerHTML = "password is not strong please provide uppercase,lowercase,special characters,digits and length of password is atleast eight";  
+                 return false;
+             }
+             else{
+                 document.getElementById("message").innerHTML = "";
+                 document.getElementById("message1").innerHTML = "";
+                 return true;
+             }
+             
+    }
+      
